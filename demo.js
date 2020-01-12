7ed86d6c9d0f744ba1512da1554a119e639aed6b
Bill Lennox <Digitalbil@gmail.com>
2020-01-12 03:24:46 -0500

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 const MixerDiscordBot = require('./index');
 
-const channelId = 665827705928810496;
+const channelId = Digitalbill;
 const webhook = 'https://discordapp.com/api/webhooks/665827143535558659/6wFRxcQiY3F569FUllmx__v9hkeI6Kc-5KlHOecxZT6xHEGiG-0j498xr1lxugB2gDaA';
 const config = { channelId, webhook };
 const bot = new MixerDiscordBot(config);
